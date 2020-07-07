f6898fbcb57295d95113d43b381354a9c4afb4f9
Dennis Hawran <56675253+AionixX@users.noreply.github.com>
2020-07-07 08:19:17 +0200

    update file

--- mm/README.mm
+++ mm/README.mm
@@ -2,7 +2,7 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1486378397436" ID="ID_1426926739" MODIFIED="1486378537951" TEXT="FreeMind Viewer&#10;Version 0.3.0">
 <node CREATED="1486378444365" ID="ID_1518824081" MODIFIED="1486379039385" POSITION="right" TEXT="Usage">
-<node CREATED="1486378547999" ID="ID_388104812" MODIFIED="1486378561523" TEXT="Hello World"/>
+<node CREATED="1486378547999" ID="ID_388104812" MODIFIED="1486378561523" TEXT="Hello World 2"/>
 <node CREATED="1486378562580" ID="ID_1243274337" MODIFIED="1486379040836" TEXT="with get-parameters">
 <node CREATED="1486378573125" ID="ID_1277953020" MODIFIED="1486378589504" TEXT="path (path to folder containing the mindmap-file"/>
 <node CREATED="1486378590257" ID="ID_939084055" MODIFIED="1486378604883" TEXT="map (name of the file including extension &quot;.mm&quot;"/>
